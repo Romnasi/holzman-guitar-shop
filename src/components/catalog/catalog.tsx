dd7329494db50760ecba5fb9a420eb487943883a
Roman Nasyrov <romnasi@gmail.com>
2022-06-03 05:45:45 +0300

    добавляет компоненты catalog. header

---
+++ src/components/catalog/catalog.tsx
@@ -0,0 +1,11 @@
+import Header from '../header/header';
+
+function Catalog(): JSX.Element {
+  return (
+    <div className="wrapper">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default Catalog;
