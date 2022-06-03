ea778e64e042839af0c15721ec8e4e4f02b83b91
Roman Nasyrov <romnasi@gmail.com>
2022-06-03 06:41:46 +0300

    добавляет компонент Breadcrumbs

--- src/components/catalog-main/catalog-main.tsx
+++ src/components/catalog-main/catalog-main.tsx
@@ -2,6 +2,7 @@ import Breadcrumbs from '../breadcrumbs/breadcrumbs';
 import CatalogCards from '../catalog-cards/catalog-cards';
 import CatalogFilter from '../catalog-filter/catalog-filter';
 import CatalogSort from '../catalog-sort/catalog-sort';
+import Pagination from '../pagination/pagination';
 
 function CatalogMain(): JSX.Element {
   return (
@@ -13,18 +14,7 @@ function CatalogMain(): JSX.Element {
           <CatalogFilter />
           <CatalogSort />
           <CatalogCards />
-          <div className="pagination page-content__pagination">
-            <ul className="pagination__list">
-              <li className="pagination__page pagination__page--active"><a className="link pagination__page-link" href="1">1</a>
-              </li>
-              <li className="pagination__page"><a className="link pagination__page-link" href="2">2</a>
-              </li>
-              <li className="pagination__page"><a className="link pagination__page-link" href="3">3</a>
-              </li>
-              <li className="pagination__page pagination__page--next" id="next"><a className="link pagination__page-link" href="2">Далее</a>
-              </li>
-            </ul>
-          </div>
+          <Pagination />
         </div>
       </div>
     </main>
