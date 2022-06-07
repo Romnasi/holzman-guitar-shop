11ed8a5ac02c7da4a642409884ee182c667981d8
Roman Nasyrov <romnasi@gmail.com>
2022-06-07 04:55:58 +0300

    добавляет 'react-router-dom'

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import CatalogPage from '../catalog-page/catalog-page';
+import NotFoundPage from '../not-found-page/not-found-page';
 
 function App(): JSX.Element {
-  return <CatalogPage />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/catalog">
+          <CatalogPage />
+        </Route>
+
+        <Route exact path="/catalog/:paginationNumber">
+          <CatalogPage />
+        </Route>
+
+        <Route path="*">
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
