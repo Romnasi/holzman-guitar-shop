73ffd8e72cadf8262bac93e23f3b16b1cc98bd9b
Roman Nasyrov <romnasi@gmail.com>
2022-06-09 02:22:43 +0300

    добавил страницу 404

--- src/components/not-found-page/not-found-page.tsx
+++ src/components/not-found-page/not-found-page.tsx
@@ -1,6 +1,31 @@
+import './not_found-page.css';
+
+import Header from '../header/header';
+import Footer from '../footer/footer';
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const/app-route';
+
 function NotFoundPage(): JSX.Element {
   return(
-    <h1>404</h1>
+    <div className="wrapper">
+      <Header/>
+
+      <main className='page-content'>
+        <div className='container not-found'>
+          <h1 className="glitch" data-text="404">
+            404
+          </h1>
+          <p className="glitch glitch--sub" data-text="Извините страница не найдена">
+            Извините страница не найдена
+          </p>
+          <Link className="button button--red not-found__button" to={AppRoute.CATALOG}>
+            Перейти на страницу каталога
+          </Link>
+        </div>
+      </main>
+
+      <Footer/>
+    </div>
   );
 }
 
