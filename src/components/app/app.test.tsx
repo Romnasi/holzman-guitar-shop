63f6ca5200438cc509478b11bc1ea370b7502a1a
Roman Nasyrov <romnasi@gmail.com>
2022-06-16 11:24:28 +0300

    добавляет тесты для комопнента App

---
+++ src/components/app/app.test.tsx
@@ -0,0 +1,58 @@
+import {render, screen} from '@testing-library/react';
+import {Router} from 'react-router-dom';
+import {createMemoryHistory} from 'history';
+import {Provider} from 'react-redux';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import { AppRoute } from '../../const/app-route';
+import App from './app';
+import { mockGuitars, mockComments } from '../../const/mock';
+
+const mockStore = configureMockStore();
+
+const store = mockStore({
+  DATA: {
+    isDataLoaded: true,
+    isCommentsLoaded: true,
+    guitars: mockGuitars,
+    comments: mockComments,
+    curPagination: 1,
+    guitarNumber: 3,
+    curGuitar: null,
+  },
+});
+
+const history = createMemoryHistory();
+const fakeApp = (
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>
+);
+
+describe('Application Routing', () => {
+  it('should render "MainPage" when user navigate to "/"', () => {
+    history.push(AppRoute.MAIN);
+    render(fakeApp);
+
+    expect(screen.getByText(/Главная/i)).toBeInTheDocument();
+  });
+
+  it('should render "Catalog page" when user navigate to "/catalog"', () => {
+    history.push(AppRoute.CATALOG);
+    render(fakeApp);
+
+    expect(screen.getByText(/Каталог гитар/i)).toBeInTheDocument();
+    expect(screen.getByText(/Фильтр/i)).toBeInTheDocument();
+    expect(screen.getByText(/Сортировать/i)).toBeInTheDocument();
+  });
+
+  it('should render "NotFoundPage" when user navigate to non-existent route', () => {
+    history.push('/non-existent-route');
+    render(fakeApp);
+
+    expect(screen.getByText('404')).toBeInTheDocument();
+    expect(screen.getByText('Извините страница не найдена')).toBeInTheDocument();
+    expect(screen.getByText('Перейти на страницу каталога')).toBeInTheDocument();
+  });
+});
