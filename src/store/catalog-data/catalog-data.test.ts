fac18ff0a7ca5c6c31fed842780df82d0b7cc806
Roman Nasyrov <romnasi@gmail.com>
2022-06-18 00:07:45 +0300

    добавляет тесты для редьюсера

---
+++ src/store/catalog-data/catalog-data.test.ts
@@ -0,0 +1,89 @@
+import { catalogData } from './catalog-data';
+import { PaginationData } from '../../const/pagination';
+import { mockGuitars, mockComments } from '../../const/mock';
+import { addComment, addCurGuitar, changeCurPagination, loadComments, loadGuitars } from '../action';
+
+const initialState = {
+  guitars: [],
+  comments: [],
+  isDataLoaded: false,
+  isCommentsLoaded: false,
+  curPagination: PaginationData.DEFAULT_ACTIVE_PAGE,
+  guitarNumber: 0,
+  curGuitar: null,
+};
+
+describe('Reducer: catalogData', () => {
+  it('without additional parameters should return initial state', () => {
+    expect(catalogData(void 0, { type: 'UNKNOWN_ACTION' }))
+      .toEqual(initialState);
+  });
+
+  it('should change guitarNumber, update guitars by load guitars', () => {
+    expect(catalogData(initialState, loadGuitars(mockGuitars)))
+      .toEqual({
+        guitars: mockGuitars,
+        comments: [],
+        isDataLoaded: true,
+        isCommentsLoaded: false,
+        curPagination: PaginationData.DEFAULT_ACTIVE_PAGE,
+        guitarNumber: mockGuitars.length,
+        curGuitar: null,
+      });
+  });
+
+  it('should update comments by load comments', () => {
+    expect(catalogData(initialState, loadComments(mockComments)))
+      .toEqual({
+        guitars: [],
+        comments: mockComments,
+        isDataLoaded: false,
+        isCommentsLoaded: true,
+        curPagination: PaginationData.DEFAULT_ACTIVE_PAGE,
+        guitarNumber: 0,
+        curGuitar: null,
+      });
+  });
+
+  it('should change current pagination', () => {
+    const testPaginationNumber = 3;
+    expect(catalogData(initialState, changeCurPagination(testPaginationNumber)))
+      .toEqual({
+        guitars: [],
+        comments: [],
+        isDataLoaded: false,
+        isCommentsLoaded: false,
+        curPagination: testPaginationNumber,
+        guitarNumber: 0,
+        curGuitar: null,
+      });
+  });
+
+  it('should update the current guitar data from selected', () => {
+    const testCurGuitar = mockGuitars[0];
+    expect(catalogData(initialState, addCurGuitar(testCurGuitar)))
+      .toEqual({
+        guitars: [],
+        comments: [],
+        isDataLoaded: false,
+        isCommentsLoaded: false,
+        curPagination: PaginationData.DEFAULT_ACTIVE_PAGE,
+        guitarNumber: 0,
+        curGuitar: testCurGuitar,
+      });
+  });
+
+  it('should add comment', () => {
+    const testComment = mockComments[0];
+    expect(catalogData(initialState, addComment(testComment)))
+      .toEqual({
+        guitars: [],
+        comments: [testComment],
+        isDataLoaded: false,
+        isCommentsLoaded: false,
+        curPagination: PaginationData.DEFAULT_ACTIVE_PAGE,
+        guitarNumber: 0,
+        curGuitar: null,
+      });
+  });
+});
