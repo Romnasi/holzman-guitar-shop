a58bbbf7f75ace1a65430e68f342f7c107fa78d6
Roman Nasyrov <romnasi@gmail.com>
2022-06-17 17:39:10 +0300

    исправляет ошибки в тестах для Contacts и Footer

--- src/components/contacts/contacts.test.tsx
+++ src/components/contacts/contacts.test.tsx
@@ -4,9 +4,9 @@ import Contacts from './contacts';
 describe('Component: Contacts', () => {
   it('should render correctly', () => {
     render(<Contacts />);
-  });
 
-  expect(screen.getByText('Контакты')).toBeInTheDocument();
-  expect(screen.getByText('г. Санкт-Петербург')).toBeInTheDocument();
-  expect(screen.getByText('Режим работы')).toBeInTheDocument();
+    expect(screen.getByText('Контакты')).toBeInTheDocument();
+    expect(screen.getByText(/Санкт-Петербург/i)).toBeInTheDocument();
+    expect(screen.getByText(/Режим работы/i)).toBeInTheDocument();
+  });
 });
--- src/components/footer/footer.test.tsx
+++ src/components/footer/footer.test.tsx
@@ -1,13 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import Footer from './footer';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+const history = createMemoryHistory();
 
 describe('Component: Contacts', () => {
   it('should render correctly', () => {
-    render(<Footer />);
-  });
+    render(
+      <Router history={history}>
+        <Footer />
+      </Router>);
 
-  expect(screen.getByAltText('Логотип')).toBeInTheDocument();
-  expect(screen.getByText('Контакты')).toBeInTheDocument();
-  expect(screen.getByText('Информация')).toBeInTheDocument();
-  expect(screen.getByText('Режим работы')).toBeInTheDocument();
+    expect(screen.getByAltText(/Логотип/i)).toBeInTheDocument();
+    expect(screen.getByText(/Контакты/i)).toBeInTheDocument();
+    expect(screen.getByText(/Информация/i)).toBeInTheDocument();
+    expect(screen.getByText(/Режим работы/i)).toBeInTheDocument();
+  });
 });
