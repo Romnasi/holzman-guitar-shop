bd6dec5d466263cac2d75b769671d9580a0cb830
Roman Nasyrov <romnasi@gmail.com>
2022-06-29 09:14:11 +0300

    фиксит упавшие тесты после добавления нового компонента

--- src/components/not-found-page/not-found-page.test.tsx
+++ src/components/not-found-page/not-found-page.test.tsx
@@ -1,15 +1,30 @@
-import {render, screen} from '@testing-library/react';
-import {Router} from 'react-router-dom';
-import {createMemoryHistory} from 'history';
+import { render, screen } from '@testing-library/react';
 import NotFoundPage from './not-found-page';
+import { Router } from 'react-router-dom';
+import {createMemoryHistory} from 'history';
+import { mockGuitars } from '../../const/mock';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { Provider } from 'react-redux';
+
+const mockStore = configureMockStore();
+const history = createMemoryHistory();
+
+const storeWithData = mockStore({
+  DATA: {
+    isDataLoaded: true,
+    isCommentsLoaded: true,
+    guitars: mockGuitars,
+  },
+});
 
 describe('Component: NotFoundScreen', () => {
   it('should render correctly', () => {
-    const history = createMemoryHistory();
     render(
-      <Router history={history}>
-        <NotFoundPage />
-      </Router>,
+      <Provider store={storeWithData}>
+        <Router history={history}>
+          <NotFoundPage />
+        </Router>
+      </Provider>,
     );
 
     expect(screen.getByText('404')).toBeInTheDocument();
