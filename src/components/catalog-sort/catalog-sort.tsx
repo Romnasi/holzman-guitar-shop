b008e0fbc765818e6b95f6233ef5d5b6e6736d63
Roman Nasyrov <romnasi@gmail.com>
2022-06-30 02:01:29 +0300

    добавляет изменение стейта сортировки и изменение классов в разметке

--- src/components/catalog-sort/catalog-sort.tsx
+++ src/components/catalog-sort/catalog-sort.tsx
@@ -1,24 +1,88 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { getSortType } from '../../store/catalog-data/selectors';
+import { changeSortType } from '../../store/action';
+import { SortDirection, SortName } from '../../const/sort';
+import { CatalogSortType } from '../../types/card-data';
+
+const getSortButtonClass = (buttonType: string, sortState: CatalogSortType) => {
+  const { type, direction, isActive} = sortState;
+  let typeButtonClass = 'catalog-sort__type-button';
+  let upOrderButtonClass = 'catalog-sort__order-button catalog-sort__order-button--up';
+  let downOrderButtonClass = 'catalog-sort__order-button catalog-sort__order-button--down';
+  const activeOrderButtonClass = 'catalog-sort__order-button--active';
+
+  switch (buttonType) {
+    case SortName.PRICE:
+      typeButtonClass = isActive && type === SortName.PRICE
+        ? `${typeButtonClass} ${typeButtonClass}--active`
+        : typeButtonClass;
+      return typeButtonClass;
+    case SortName.COMMENT:
+      typeButtonClass = isActive && type === SortName.COMMENT
+        ? `${typeButtonClass} ${typeButtonClass}--active`
+        : typeButtonClass;
+      return typeButtonClass;
+    case SortDirection.ASCENDING:
+      upOrderButtonClass = isActive && direction === SortDirection.ASCENDING
+        ? `${upOrderButtonClass} ${activeOrderButtonClass}`
+        : upOrderButtonClass;
+      return upOrderButtonClass;
+    case SortDirection.DESCENDING:
+      downOrderButtonClass = isActive && direction === SortDirection.DESCENDING
+        ? `${downOrderButtonClass} ${activeOrderButtonClass}`
+        : downOrderButtonClass;
+      return downOrderButtonClass;
+    default:
+      return '';
+  }
+};
+
 function CatalogSort(): JSX.Element {
+  const sortType = useSelector(getSortType);
+  const dispatch = useDispatch();
+
   return (
     <div className="catalog-sort">
       <h2 className="catalog-sort__title">Сортировать:</h2>
       <div className="catalog-sort__type">
-        <button className="catalog-sort__type-button" aria-label="по цене">
+        <button
+          onClick={() => {
+            const update = { type: SortName.PRICE, isActive: true };
+            dispatch(changeSortType({...sortType, ...update}));
+          }}
+          className={getSortButtonClass(SortName.PRICE, sortType)}
+          aria-label="по цене"
+        >
           по цене
         </button>
-        <button className="catalog-sort__type-button" aria-label="по популярности">
+        <button
+          onClick={() => {
+            const update = { type: SortName.COMMENT, isActive: true };
+            dispatch(changeSortType({...sortType, ...update}));
+          }}
+          className={getSortButtonClass(SortName.COMMENT, sortType)}
+          aria-label="по популярности"
+        >
           по популярности
         </button>
       </div>
 
       <div className="catalog-sort__order">
         <button
-          className="catalog-sort__order-button catalog-sort__order-button--up"
+          onClick={() => {
+            const update = { direction: SortDirection.ASCENDING, isActive: true };
+            dispatch(changeSortType({...sortType, ...update}));
+          }}
+          className={getSortButtonClass(SortDirection.ASCENDING, sortType)}
           aria-label="По возрастанию"
         >
         </button>
         <button
-          className="catalog-sort__order-button catalog-sort__order-button--down"
+          onClick={() => {
+            const update = { direction: SortDirection.DESCENDING, isActive: true };
+            dispatch(changeSortType({...sortType, ...update}));
+          }}
+          className={getSortButtonClass(SortDirection.DESCENDING, sortType)}
           aria-label="По убыванию"
         >
         </button>
