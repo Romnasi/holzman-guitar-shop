3c1f4a6ea171e6eee583811d6bbb6c04947fa820
Roman Nasyrov <romnasi@gmail.com>
2022-07-03 17:04:08 +0300

    разделяет фильтр на компоненты

--- src/components/catalog-filter/catalog-filter.tsx
+++ src/components/catalog-filter/catalog-filter.tsx
@@ -2,6 +2,8 @@
 import { useSelector } from 'react-redux';
 import { getGuitars } from '../../store/catalog-data/selectors';
 import FilterPrice from '../filter-price/filter-price';
+import FilterType from '../../filter-type/filter-type';
+import FilterStrings from '../filter-strings/filter-strings';
 
 function CatalogFilter(): JSX.Element {
   const guitars = useSelector(getGuitars);
@@ -14,41 +16,9 @@ function CatalogFilter(): JSX.Element {
         guitars={guitars}
       />
 
-      <fieldset className="catalog-filter__block">
-        <legend className="catalog-filter__block-title">Тип гитар</legend>
-        <div className="form-checkbox catalog-filter__block-item">
-          <input className="visually-hidden" type="checkbox" id="acoustic" name="acoustic" />
-          <label htmlFor="acoustic">Акустические гитары</label>
-        </div>
-        <div className="form-checkbox catalog-filter__block-item">
-          <input className="visually-hidden" type="checkbox" id="electric" name="electric" />
-          <label htmlFor="electric">Электрогитары</label>
-        </div>
-        <div className="form-checkbox catalog-filter__block-item">
-          <input className="visually-hidden" type="checkbox" id="ukulele" name="ukulele" />
-          <label htmlFor="ukulele">Укулеле</label>
-        </div>
-      </fieldset>
+      <FilterType />
 
-      <fieldset className="catalog-filter__block">
-        <legend className="catalog-filter__block-title">Количество струн</legend>
-        <div className="form-checkbox catalog-filter__block-item">
-          <input className="visually-hidden" type="checkbox" id="4-strings" name="4-strings" />
-          <label htmlFor="4-strings">4</label>
-        </div>
-        <div className="form-checkbox catalog-filter__block-item">
-          <input className="visually-hidden" type="checkbox" id="6-strings" name="6-strings" />
-          <label htmlFor="6-strings">6</label>
-        </div>
-        <div className="form-checkbox catalog-filter__block-item">
-          <input className="visually-hidden" type="checkbox" id="7-strings" name="7-strings" />
-          <label htmlFor="7-strings">7</label>
-        </div>
-        <div className="form-checkbox catalog-filter__block-item">
-          <input className="visually-hidden" type="checkbox" id="12-strings" name="12-strings" />
-          <label htmlFor="12-strings">12</label>
-        </div>
-      </fieldset>
+      <FilterStrings />
 
       <button
         className="catalog-filter__reset-btn button button--black-border button--medium"
